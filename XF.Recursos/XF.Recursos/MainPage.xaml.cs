2ab2e37d4807d4a7c4364b4ff9a864cb3d2b87dc
Eduardo <eduardo.dnunes@hotmail.com>
2017-10-28 14:21:42 -0200

    Messaging Center (Parte 2)

--- XF.Recursos/XF.Recursos/MainPage.xaml.cs
+++ XF.Recursos/XF.Recursos/MainPage.xaml.cs
@@ -58,5 +58,15 @@ namespace XF.Recursos
         {
             await Navigation.PushAsync(new Lista.ListaEtiquetaView());
         }
+
+        private async void btnHome_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PassParameter.HomeView(DateTime.Now.ToString("u")));
+        }
+
+        private async void btnMC_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PassParameter.MCHomeView());
+        }
     }
 }
--- XF.Recursos/XF.Recursos/PassParameter/MCHomeView.xaml.cs
+++ XF.Recursos/XF.Recursos/PassParameter/MCHomeView.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XF.Recursos.Global;
 
 namespace XF.Recursos.PassParameter
 {
